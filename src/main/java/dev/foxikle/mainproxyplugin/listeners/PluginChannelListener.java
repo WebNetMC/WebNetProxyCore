533e512a585fb815ec1f89d501764c50015b40d0
Foxikle <tristantroop260@gmail.com>
2024-01-20 09:58:41 -0500

    Add debug message

--- src/main/java/dev/foxikle/mainproxyplugin/listeners/PluginChannelListener.java
+++ src/main/java/dev/foxikle/mainproxyplugin/listeners/PluginChannelListener.java
@@ -5,6 +5,7 @@ import com.velocitypowered.api.event.connection.PluginMessageEvent;
 import com.velocitypowered.api.proxy.Player;
 import dev.foxikle.mainproxyplugin.MainProxy;
 import dev.foxikle.mainproxyplugin.utils.ServerUtils;
+import net.kyori.adventure.text.Component;
 
 public class PluginChannelListener {
     private final MainProxy plugin;
@@ -19,6 +20,7 @@ public class PluginChannelListener {
             // main channel things
         } else if (event.getIdentifier() == MainProxy.LOBBY_REQUEST) {
            if(event.getSource() instanceof Player player) {
+               player.sendMessage(Component.text("Yo ur gettin transfered!"));
                plugin.getProxy().getScheduler().buildTask(plugin, () -> ServerUtils.connectToServer("lobby", plugin.getProxy(), player)).schedule();
            }
         }
